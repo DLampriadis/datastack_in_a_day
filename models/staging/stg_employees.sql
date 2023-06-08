d5be6e6f3cb85e82c97c076eaf14a2cde05e7869
DimLampriadis <91012882+DLampriadis@users.noreply.github.com>
2023-06-08 14:04:57 +0200

    fixing errors and adding new features in the models

--- models/staging/stg_employees.sql
+++ models/staging/stg_employees.sql
@@ -3,4 +3,5 @@ select
     ,   date1 as starting_date
     ,   date2 as ending_date
     ,   team
+    ,   datediff(DAY, starting_date, CURRENT_DATE()) as days_at_work
 from {{ source('fivetran_raw_data', 'employees') }}
